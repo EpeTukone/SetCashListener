e26d757f3acc78c18a1498e286f845844a357565
EpeTukone <fren.ilya@tut.by>
2017-04-17 17:08:30 +0300

    ++sql

--- Sql query.py
+++ Sql query.py
@@ -4,12 +4,15 @@ import postgresql.driver as pg_driver
 db = pg_driver.connect(
     user='postgres',
     password='postgres',
-    host='192.168.1.253',
+    host='192.168.1.222',
     database='set',
     port=5432
     )
-cash = db.query("SELECT shop_number, number, trim(fiscalnum) FROM cash_cash WHERE status = 'ACTIVE'"
+cash = db.query("SELECT shop_number, number, trim(fiscalnum), trim(fwversion) FROM cash_cash WHERE status = 'ACTIVE'"
                 " ORDER BY shop_number, number ")
+count_cash = 0
 for i in cash:
     print(str(i))
+    count_cash += 1
 db.close()
+print(count_cash)
